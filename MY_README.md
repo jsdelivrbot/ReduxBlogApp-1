cb6323a5fae295d6f6de03fd88808cdeaf69530f
Joe Stowers <greenchiletexan@gmail.com>
2017-03-03 21:47:33 -0700

    Update MY_README

--- MY_README.md
+++ MY_README.md
@@ -21,13 +21,14 @@ The key provides a simple authentication and ensures that your posts are kept in
     
     reduxblog.herokuapp.com/api/posts?key=1a2b3c
 
-Four Available Routes
+***Four Available Routes***
 
-    Endpoint        Method      Route
-1.  /api/posts      GET         index => returns an array of blog posts
-2.  /api/posts      POST        create blog post and return saved version
-3.  /api/posts/:id  GET         return a single post based on the id
-4.  /api/posts/:id  DELETE      delete a single post based on the id
+|  Endpoint        |  Method    |  Route             
+| ---------------- |:----------:| ------------------:                         
+|  /api/posts      |  GET       |  index => returns an array of blog posts
+|  /api/posts      |  POST      |  create blog post and return saved version
+|  /api/posts/:id  |  GET       |  return a single post based on the id
+|  /api/posts/:id  |  DELETE    |  delete a single post based on the id
 
 NOTE:  a GET request on an individual blog post returns the content of the post; whereas a GET request on the index does not return any content.
 
--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,46 @@
-routes.js
+// Route Configuration
+// Sec. 6, Lec. 74
+// Thursday, March 2, 2017
+
+// routes.js will provide a mapping mechanism 
+// to display specified components for each URL
+
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import App from './components/app';
+
+const Greeting = () => {
+	return <div>Hey There</div>;
+};
+
+//	Syntax: 
+//		whenever the user is at this path, show this component
+// 		<Route path = "/" component = { insertName } />
+export default (
+
+	// '/' refers to the root domain name
+	// google.com/ => renders App
+	<Route path = "/" component = { App } >
+		<Route path = "greet1" component = { Greeting } />
+		<Route path = "greet2" component = { Greeting } />
+		<Route path = "greet3" component = { Greeting } />
+	</Route>
+);
+
+/* 	Example
+	--------
+	Nested Components under the root path
+
+	// Form template
+	const Greeting = () => {
+		return <div>Hey There</div>;
+	};
+
+	// Nested components
+	<Route path = "/" component = { App } >
+		<Route path = "greet1" component = { Greeting } />
+		<Route path = "greet2" component = { Greeting } />
+		<Route path = "greet3" component = { Greeting } />
+	</Route>
+*/
+
