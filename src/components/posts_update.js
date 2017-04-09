bd4fb80f0fdcf80391c32962d950e1104bd11178
Joe Stowers <greenchiletexan@gmail.com>
2017-04-08 19:06:49 -0600

    Completed basic functionality of posts_update component

--- src/components/posts_update.js
+++ src/components/posts_update.js
@@ -1,24 +1,141 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { updatePost } from '../actions/index';
+import { createPost } from '../actions/index';
+import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router';
+
+import {
+	Button,
+	ButtonGroup,
+	ButtonToolbar,
+	Col,
+	ControlLabel,
+	FormControl,
+	FormGroup
+} from 'react-bootstrap';
+
+// function to render each field
+const renderField = ({ type, input, meta: { touched, error }, 
+						placeholder, componentClass, label }) => {
+
+	console.log('input =', input);
+
+	return(
+		<div>
+			<FormGroup
+				controlId= {input.name}
+				validationState= {touched ? 'error' : 'success'}>
+				<ControlLabel className="form-label">{label}</ControlLabel>
+				<FormControl 
+					className= {input.name}
+					type= {type}
+					componentClass= {componentClass}
+					placeholder= {placeholder}
+					value= {input.value}
+					onChange= {input.onChange}
+				/>
+				<div className="form-field-error">
+					{error && touched && <span>{error}</span>}
+				</div>
+				<FormControl.Feedback />
+			</FormGroup>
+		</div>
+	);
+}
 
 class PostsUpdate extends Component {
 
+	// PostsNew.contextTypes returns object below
+	static contextTypes = {
+		router: PropTypes.object
+	};
+
+	onSubmit = (props) => {
+		this.props.createPost(props)
+		  .then(() => { this.context.router.push('/') });
+	}
+
 	render() {
 
-		const { post } = this.props;
-
-		console.log('posts_update props = ', post);
+		const { handleSubmit } = this.props;
 
 		return (
-			<h3>Inside Posts Update</h3>
+			<form onSubmit = { handleSubmit(this.onSubmit) }>
+				<div className = "new-post-header">
+					<h3>Update a Post</h3>
+				</div>
+				<Field
+					name= "title"
+					component= { renderField }
+					type= "text"
+					props= {{ label:'Title' }}
+					placeholder= "Make it groovy">
+				</Field>
+				<Field
+					name= "categories"
+					component= { renderField }
+					type= "text"
+					props= {{ label:'Categories' }}
+					placeholder= "Organize for later">
+				</Field>
+				<Field
+					name= "content"
+					component= { renderField }
+					type= "textarea"
+					props= {{ componentClass: 'textarea', label:'Content' }}
+					placeholder= "Your thoughts matter">
+				</Field>
+				<ButtonToolbar>
+					<Button type="submit"
+							bsStyle="primary">Submit
+			        </Button>
+			        <Button bsStyle="danger">
+		        		<Link to= "/"
+		        			  style={{color: 'white', 'textDecoration':'none'}}>Cancel
+	        			</Link>
+        			</Button>
+				</ButtonToolbar>
+			</form>
 		);
 	}
 }
 
+// Form validation
+const validate = values => {
 
-function mapStateToProps(state) {
-	return { post: state.posts.post };
+	const errors = {};
+
+	if(!values.title) {
+		errors.title = 'Enter a groovy blog title.';
+	}
+	if(!values.categories) {
+		errors.categories = 'Enter some categories.';
+	}
+	if(!values.content) {
+		errors.content = 'No content, no submit.';
+	}
+
+	// return an object
+	return errors;
 }
 
-export default connect(mapStateToProps, null)(PostsUpdate);
+function mapStateToProps(state) {
+
+	// create initialValues object of properties that will populate
+	// value property of Redux Field object.
+	return { 
+		initialValues: {
+			id: state.posts.post.id,
+			title: state.posts.post.title,
+			categories: state.posts.post.categories,
+			content: state.posts.post.content
+		}
+	};
+}
+
+PostsUpdate = reduxForm({
+	form:'PostsNewForm',
+	validate
+})(PostsUpdate);
+
+export default connect(mapStateToProps, { createPost })(PostsUpdate);
